b930a655d15ba0e7e0c26a1dbbdfb35aa41581ea
Anthony Perez <antrp93@gmail.com>
2020-05-06 20:44:39 -0500

    Review model added

---
+++ Models/Review.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace capstone.Models
+{
+    public class Review
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+
+        public string[] Comments {get; set;}
+    }
+}
